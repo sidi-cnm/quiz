a21fdc4db95a60274b65611d1c022d970def5dfb
sidi-cnm <sdeiebeidy@gmail.com>
2025-12-18 16:50:20 +0000

    first commit

---
+++ executable.py
@@ -0,0 +1,10 @@
+import quiz
+
+# Graphique en ligne simple
+quiz.styled_line(
+    x=[1, 2, 3, 4, 5],
+    y=[2, 4, 3, 5, 7],
+    title="Mon premier graphique stylisé",
+    xlabel="Axe X",
+    ylabel="Axe Y"
+)
